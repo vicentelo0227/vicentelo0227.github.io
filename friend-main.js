ce8f2c803f12e298364fdbc3b2403aab45b36dab
vicentelo0227 <60609906vincent@gmail.com>
2024-05-26 10:20:29 +0800

    friend-main.js

--- friend-main.js
+++ friend-main.js
@@ -1,119 +1,152 @@
-<!DOCTYPE html>
-<html lang="en">
+const BASE_URL = "https://user-list.alphacamp.io"
+const INDEX_URL = BASE_URL + "/api/v1/users"
+const SHOW_URL = BASE_URL + "/api/v1/users/"
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <!-- Bootstrap的樣式 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <!-- 我自己的樣式 -->
-  <link rel="stylesheet" href="friend-style.css">
-</head>
+const friends = []
+let filterFriends = []
+const pageDisplayNumber = 24
 
-<body class="bg-secondary-subtle">
-  <!-- friend Navbar -->
-  <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">C2-KEY-HW</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#" id="home">
-              Home
-              <span class="visually-hidden">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link favoriteBtn" href="#">Favorite</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+const dataPanel = document.querySelector('#data-panel')
+const pagination = document.querySelector('#paginator')
+const submitButton = document.querySelector('#submitButton')
+const friendNameKey = document.querySelector('#friendNameKey')
+const home = document.querySelector('#home')
+const navbar = document.querySelector('nav')
 
-
-  <div class="container-fluid mt-3">
-
-    <!-- searchbar -->
-    <form class="row row-cols-lg-auto g-3 align-items-center justify-content-center" id="submitButton">
-
-      <div class="col-4">
-        <label class="visually-hidden" for="inlineFormInputGroupFriendname">friend name...
-        </label>
-        <div class="input-group">
-          <input type="text" class="form-control text-center" id="friendNameKey" placeholder="friend name...">
-        </div>
-      </div>
-
-      <div class="col-4">
-        <button type="submit" class="btn btn-info form-control text-center">Search</button>
-      </div>
-
-    </form>
-
-
-    <div class="row mt-3" id="data-panel">
-      <!-- Render friends list -->
-    </div>
-  </div>
-
-  <!-- pagination -->
-  <nav aria-label="Page navigation example" class="sticky-bottom">
-    <ul class="pagination justify-content-center" id="paginator">
-      <!-- pagination Number Render -->
-    </ul>
-  </nav>
-
-  <!-- friend Modal -->
-  <div class="modal" tabindex="-1" id="friendDetailsModal">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-header" style="background-color: #e3f2fd;">
-          <h5 class="modal-title">Test Name</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div class="row">
-            <div class="friendPicture col-sm-6">
-              <img src="" alt="selfie" class="img-fluid">
-            </div>
-            <div class="introtext col-sm-6 d-flex align-items-center">
-              <div>
-                <p class="email"></p>
-                <p class="gender"></p>
-                <p class="age"></p>
-                <p class="region"></p>
-                <p class="birthday"></p>
-              </div>
-            </div>
+// 渲染朋友資料畫面
+function renderFriendList(Data) {
+  let friendListHTML = ""
+  Data.forEach((friend) => {
+    friendListHTML += `
+      <div class="col-sm-2">
+        <div class="mb-2 mt-0 ms-0">
+          <div class="card mb-1">
+            <img src=${friend.avatar} class="selfie img-fluid border border-secondary"
+              alt="friend-selfie" type="button" class="btn btn-primary" data-bs-toggle="modal"
+              data-bs-target="#friendDetailsModal" data-id=${friend.id}>
+          </div>
+          <div class="card-footer">
+            <p class="friendNameSurname text-center border border-secondary" type="button" style="background-color: #e3f2fd;" data-id=${friend.id}>${friend.name} ${friend.surname}</p>
           </div>
         </div>
-        <div class="modal-footer" style="background-color: #e3f2fd;">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-add-favorite btn-info">Add to BestFriends</button>
-        </div>
       </div>
-    </div>
-  </div>
+    `
+  });
+  dataPanel.innerHTML = friendListHTML
+}
+
+//朋友介紹模塊
+function uniqueFriendModal(id) {
+  const friendName = document.querySelector('.modal-title')
+  const email = document.querySelector('.email')
+  const gender = document.querySelector('.gender')
+  const age = document.querySelector('.age')
+  const region = document.querySelector('.region')
+  const birthday = document.querySelector('.birthday')
+  const friendPicture = document.querySelector('.friendPicture')
+
+  axios.get(SHOW_URL + id).then(response => {
+    const friendObject = response.data
+    friendName.innerText = `${friendObject.name} ${friendObject.surname}`
+    email.innerText = "Email: " + friendObject.email
+    gender.innerText = "Gender: " + friendObject.gender
+    age.innerText = "Age: " + friendObject.age
+    region.innerText = "Region: " + friendObject.region
+    birthday.innerText = "Birthday: " + friendObject.birthday
+    friendPicture.innerHTML = `
+      <img src=${friendObject.avatar} alt="selfie" class="modalSelfie">
+    `
+  })
+}
+
+//分頁器要有多少頁，以及一些格式設定
+function pageQuantity(amount) {
+  const allPageNum = Math.ceil(amount / pageDisplayNumber)
+  let paginatorHTML = ''
+
+  for (let page = 1; page <= allPageNum; page++) {
+    paginatorHTML += `
+      <li class="page-item"><a class="page-link pageNum text-black" href="#">${page}</a></li>
+    `
+  }
+  pagination.innerHTML = paginatorHTML
+}
 
 
-  <!-- axios -->
-  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+function pageNumActive(event) {
+  const target = event.target
+  const pageNum = document.querySelectorAll('.pageNum')
+  pageNum.forEach((num) => {
+    num.classList.remove('active', 'text-white')
+    num.classList.add('text-black')
+  })
+  target.classList.remove('text-black')
+  target.classList.add('active', 'text-white')
+}
 
-  <!-- myself -->
-  <script src="friend-main.js"></script>
+
+//渲染每一頁的呈現資料
+function renderEachPage(pageNumber) {
+  const data = filterFriends.length ? filterFriends : friends
+  // 註記，這裡記得用length當作存在東西，根據導流的資料是有查詢的，還是沒查詢的分野
+
+  const currentPage = pageNumber
+  const dataStart = (currentPage - 1) * pageDisplayNumber
+  const dataEnd = currentPage * pageDisplayNumber
+  const dataToDisplay = data.slice(dataStart, dataEnd)
+
+  renderFriendList(dataToDisplay)
+}
 
 
-  <!-- bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
-</body>
 
-</html>
+// 提交查詢事件
+submitButton.addEventListener('submit', function onSubmitClicked(event) {
+  event.preventDefault()
+  const keyWord = friendNameKey.value.trim().toLowerCase()
+
+  filterFriends = friends.filter((friend) => friend.name.toLowerCase().includes(keyWord) || friend.surname.toLowerCase().includes(keyWord))
+
+  if (filterFriends.length === 0) {
+    return alert('No matches for ' + keyWord)
+  }
+
+  pageQuantity(filterFriends.length)
+  renderEachPage(1) // 查詢結果第一頁的東西
+})
+
+
+// 點擊大頭貼事件，觸發跳出介紹框框
+dataPanel.addEventListener('click', function triggerFriendModal(event) {
+  if (event.target.matches('.selfie')) {
+    uniqueFriendModal(Number(event.target.dataset.id))
+  }
+})
+
+// 點擊分頁數字觸發事件
+pagination.addEventListener('click', function onPanelClicked(event) {
+  event.preventDefault()
+  if (event.target.matches('.pageNum')) {
+    renderEachPage(Number(event.target.innerText))
+    pageNumActive(event)
+  }
+})
+
+// 點home或主頁大標題回預設畫面第一頁
+navbar.addEventListener('click', function onNavClicked(event) {
+  if (!event.target.matches('.favoriteBtn')) {
+    window.location.reload()
+  }
+})
+
+
+// Call Friend API
+axios.get(INDEX_URL).then(response => {
+  friends.push(...response.data.results)
+
+  pageQuantity(friends.length)
+  renderEachPage(1)
+})
+  .catch((err) => {
+    console.log(err)
+  })
