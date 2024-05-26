630336e529982ec8404a868b0e9d7b61faf7eb04
vicentelo0227 <60609906vincent@gmail.com>
2024-05-26 10:19:28 +0800

    friend-main.js

--- friend-main.js
+++ friend-main.js
@@ -1,77 +1,119 @@
-const BASE_URL = "https://user-list.alphacamp.io"
-const INDEX_URL = BASE_URL + "/api/v1/users"
-const SHOW_URL = BASE_URL + "/api/v1/users/"
+<!DOCTYPE html>
+<html lang="en">
 
-const dataPanel = document.querySelector('#data-panel')
-const friends = []
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Bootstrap的樣式 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- 我自己的樣式 -->
+  <link rel="stylesheet" href="friend-style.css">
+</head>
 
-function renderFriendList(data) {
-  let friendListHTML = ""
-  data.forEach(friend => {
-    friendListHTML += `
-      <div class="col-sm-1">
-        <div class="mb-2 mt-0 ms-0">
-          <div class="card">
-            <img src=${friend.avatar} class="card-img-top selfie"
-              alt="friend-selfie" type="button" class="btn btn-primary" data-bs-toggle="modal"
-              data-bs-target="#friendDetailsModal" data-id=${friend.id}>
-          </div>
-            <div class="card-footer">
-              <p class="friendNameSurname" type="button" data-id=${friend.id}>${friend.name} ${friend.surname}</p>
-            </div>
+<body class="bg-secondary-subtle">
+  <!-- friend Navbar -->
+  <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">C2-KEY-HW</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#" id="home">
+              Home
+              <span class="visually-hidden">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link favoriteBtn" href="#">Favorite</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <div class="container-fluid mt-3">
+
+    <!-- searchbar -->
+    <form class="row row-cols-lg-auto g-3 align-items-center justify-content-center" id="submitButton">
+
+      <div class="col-4">
+        <label class="visually-hidden" for="inlineFormInputGroupFriendname">friend name...
+        </label>
+        <div class="input-group">
+          <input type="text" class="form-control text-center" id="friendNameKey" placeholder="friend name...">
         </div>
       </div>
-    `
-  });
-  dataPanel.innerHTML = friendListHTML
-}
 
-function uniqueFriendModal(id) {
-  const friendName = document.querySelector('.modal-title')
-  const email = document.querySelector('.email')
-  const gender = document.querySelector('.gender')
-  const age = document.querySelector('.age')
-  const region = document.querySelector('.region')
-  const birthday = document.querySelector('.birthday')
-  const friendPicture = document.querySelector('.friendPicture')
+      <div class="col-4">
+        <button type="submit" class="btn btn-info form-control text-center">Search</button>
+      </div>
 
-  axios.get(SHOW_URL + id).then(response => {
-    const friendObject = response.data
-    friendName.innerText = `${friendObject.name} ${friendObject.surname}`
-    email.innerText = "Email: " + friendObject.email
-    gender.innerText = "Gender: " + friendObject.gender
-    age.innerText = "Age: " + friendObject.age
-    region.innerText = "Region: " + friendObject.region
-    birthday.innerText = "Birthday: " + friendObject.birthday
-    friendPicture.innerHTML = `
-      <img src=${friendObject.avatar} alt="selfie" class="modalSelfie">
-    `
-  })
-}
-
-function favoriteFriendList(id) {
+    </form>
 
 
-}
+    <div class="row mt-3" id="data-panel">
+      <!-- Render friends list -->
+    </div>
+  </div>
+
+  <!-- pagination -->
+  <nav aria-label="Page navigation example" class="sticky-bottom">
+    <ul class="pagination justify-content-center" id="paginator">
+      <!-- pagination Number Render -->
+    </ul>
+  </nav>
+
+  <!-- friend Modal -->
+  <div class="modal" tabindex="-1" id="friendDetailsModal">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header" style="background-color: #e3f2fd;">
+          <h5 class="modal-title">Test Name</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="friendPicture col-sm-6">
+              <img src="" alt="selfie" class="img-fluid">
+            </div>
+            <div class="introtext col-sm-6 d-flex align-items-center">
+              <div>
+                <p class="email"></p>
+                <p class="gender"></p>
+                <p class="age"></p>
+                <p class="region"></p>
+                <p class="birthday"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer" style="background-color: #e3f2fd;">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-add-favorite btn-info">Add to BestFriends</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
-// 這裡有一部份去做test
-dataPanel.addEventListener('click', function triggerFriendModal(event) {
-  if (event.target.matches('.selfie')) {
-    uniqueFriendModal(Number(event.target.dataset.id))
-  } else if (event.target.matches('.friendNameSurname')) {
-    favoriteFriendList(Number(event.target.dataset.id))
-  }
-})
+  <!-- axios -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+  <!-- myself -->
+  <script src="friend-main.js"></script>
 
 
-axios.get(INDEX_URL).then(response => {
-  friends.push(...response.data.results)
-  renderFriendList(friends)
-})
-  .catch((err) => {
-    console.log(err)
-  })
+  <!-- bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
 
-// above TEST 
-
+</html>
